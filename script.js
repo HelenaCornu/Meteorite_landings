14aabe40a3838510ced477e39105c127a51542a3
Carlos Cruz <me@carcruzcast.com>
2023-08-15 10:50:21 +0100

    add leaflet lib

---
+++ script.js
@@ -0,0 +1,20 @@
+// init map
+var map = L.map("map").setView([51.505, -0.09], 13);
+// add tile layer
+L.tileLayer(
+  "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
+  {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: "abcd",
+    maxZoom: 20,
+  }
+).addTo(map);
+
+// add marker
+var circle = L.circle([51.508, -0.11], {
+  color: "red",
+  fillColor: "#f03",
+  fillOpacity: 0.5,
+  radius: 500,
+}).addTo(map);
